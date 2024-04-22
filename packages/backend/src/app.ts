7f41ade7488b657dce940e0f1af4d6ca58aebdc5
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-22 01:14:33 +0100

    ➕ add routes -> auth route  to app route

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
 import ProcessRoute from "./routes/process.js"
+import AuthRoute from "./routes/auth/index.js"
 import { auth, notFound } from "./middleware/index.js"
 
 const AppRoute = express()
@@ -12,6 +13,8 @@ AppRoute.use(bodyParser.urlencoded({ extended: true }))
 AppRoute.use(auth())
 
 AppRoute.use("/process", ProcessRoute)
+
+AppRoute.use("/auth", AuthRoute)
 
 AppRoute.get("/", (_, res) => {
     res.redirect("/process")
