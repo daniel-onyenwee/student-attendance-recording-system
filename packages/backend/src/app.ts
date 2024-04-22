3fbcbcfc80f03be35e6186db693ed86d9cebadcb
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-22 01:13:00 +0100

    ➕ inject middlewares -> bodyParser, auth, authAccess and notFound to app

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,12 +1,22 @@
 import express from "express"
+import bodyParser from "body-parser"
 import ProcessRoute from "./routes/process.js"
+import { auth, notFound } from "./middleware/index.js"
 
 const AppRoute = express()
+
+AppRoute.use(bodyParser.json())
+
+AppRoute.use(bodyParser.urlencoded({ extended: true }))
+
+AppRoute.use(auth())
 
 AppRoute.use("/process", ProcessRoute)
 
 AppRoute.get("/", (_, res) => {
     res.redirect("/process")
 })
+
+AppRoute.use(notFound())
 
 export default AppRoute
