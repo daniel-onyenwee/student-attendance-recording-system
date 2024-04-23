8ba9bd012e541d0f0abe43f8eb179198735fe07b
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-23 15:23:59 +0100

    ➕ add route -> facultyId route to faculty route

---
+++ packages/backend/src/routes/admin/record/faculty/[facultyId].ts
@@ -0,0 +1,150 @@
+import express from "express"
+import { prismaClient } from "../../../../utils/index.js"
+import { idValidator } from "../../../../middleware/index.js"
+
+interface FacultyIDRequestBody {
+    name: string
+}
+
+const FacultyIDRoute = express.Router()
+
+FacultyIDRoute.get("/:facultyId", idValidator("facultyId"), async (req, res) => {
+    let facultyId = req.params.facultyId
+
+    let faculty = await prismaClient.faculty.findUnique({
+        where: {
+            id: facultyId
+        },
+        select: {
+            id: true,
+            name: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    if (!faculty) {
+        res.status(400)
+        res.json({
+            ok: false,
+            error: {
+                message: "Faculty not found",
+                code: 3003
+            },
+            data: null
+        })
+        return
+    }
+
+    res.status(200)
+    res.json({
+        ok: true,
+        data: faculty,
+        error: null
+    })
+})
+
+FacultyIDRoute.patch("/:facultyId", idValidator("facultyId"), async (req, res) => {
+    let facultyId = req.params.facultyId
+    let body: FacultyIDRequestBody = req.body || {}
+    body.name = body.name || String()
+    body.name = body.name
+        .toUpperCase()
+        .replace("OF", String())
+        .replace("FACULTY", String())
+        .trim()
+
+    const facultiesCount = await prismaClient.faculty.count({
+        where: {
+            id: facultyId
+        }
+    })
+
+    if (!facultiesCount) {
+        res.status(400)
+        res.json({
+            ok: false,
+            error: {
+                message: "Faculty not found",
+                code: 3003
+            },
+            data: null
+        })
+        return
+    }
+
+    let updateData: Partial<FacultyIDRequestBody> = {}
+
+    if (body.name) {
+        const facultiesCountByName = await prismaClient.faculty.count({
+            where: {
+                name: body.name,
+                id: {
+                    not: {
+                        equals: facultyId
+                    }
+                }
+            }
+        })
+
+        if (facultiesCountByName > 0) {
+            res.status(400)
+            res.json({
+                ok: false,
+                error: {
+                    message: "Faculty already exist",
+                    code: 3001
+                },
+                data: null
+            })
+            return
+        }
+
+        updateData.name = body.name
+    }
+
+    const faculty = await prismaClient.faculty.update({
+        where: {
+            id: facultyId
+        },
+        data: updateData,
+        select: {
+            name: true,
+            id: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    res.status(200)
+    res.json({
+        ok: true,
+        data: faculty,
+        error: null
+    })
+})
+
+FacultyIDRoute.delete("/:facultyId", idValidator("facultyId"), async (req, res) => {
+    let facultyId = req.params.facultyId
+
+    const faculty = await prismaClient.faculty.delete({
+        where: {
+            id: facultyId
+        },
+        select: {
+            name: true,
+            id: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    res.status(200)
+    res.json({
+        ok: true,
+        data: faculty,
+        error: null
+    })
+})
+
+export default FacultyIDRoute
---
+++ packages/backend/src/routes/admin/record/faculty/index.ts
@@ -0,0 +1,131 @@
+import express from "express"
+import { prismaClient } from "../../../../utils/index.js"
+import FacultyIDRoute from "./[facultyId].js"
+
+interface FacultyRequestBody {
+    name: string
+}
+
+const FacultyRoute = express.Router()
+
+FacultyRoute.get("/", async (req, res) => {
+    let url = new URL(req.url || String(), `http://${req.headers.host}`)
+    let name = url.searchParams.get("name") || String()
+
+    let page = +(url.searchParams.get("page") ?? 1)
+    page = !isNaN(page) ? page : 1
+    page = page > 0 ? page - 1 : 0
+
+    let count = +(url.searchParams.get("count") ?? 10)
+    count = !isNaN(count) ? count : 10
+    count = count > 0 ? count < 1000 ? count : 1000 : 10
+
+    const faculties = await prismaClient.faculty.findMany({
+        where: {
+            name: {
+                contains: name,
+                mode: "insensitive"
+            },
+        },
+        orderBy: {
+            name: "asc"
+        },
+        skip: page * count,
+        take: count,
+        select: {
+            name: true,
+            id: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    res.status(200)
+    res.json({
+        ok: true,
+        data: faculties,
+        error: null
+    })
+})
+
+FacultyRoute.post("/", async (req, res) => {
+    let body: FacultyRequestBody | null = req.body
+
+    if (!body || Object.keys(body || {}).length == 0) {
+        res.status(400)
+        res.json({
+            ok: false,
+            error: {
+                message: "Request body missing",
+                code: 1004
+            },
+            data: null
+        })
+        return
+    }
+
+    body.name = body.name || String()
+    body.name = body.name = body.name
+        .toUpperCase()
+        .replace("OF", String())
+        .replace("FACULTY", String())
+        .trim()
+
+    if (!body.name) {
+        res.status(400)
+        res.json({
+            ok: false,
+            error: {
+                message: "Missing parameter 'faculty name'",
+                code: 3000
+            },
+            data: null
+        })
+        return
+    }
+
+    const facultiesCount = await prismaClient.faculty.count({
+        where: {
+            name: {
+                equals: body.name,
+                mode: "insensitive"
+            }
+        }
+    })
+
+    if (facultiesCount > 0) {
+        res.status(400)
+        res.json({
+            ok: false,
+            error: {
+                message: "Faculty already exist",
+                code: 3001
+            },
+            data: null
+        })
+        return
+    }
+
+    const faculty = await prismaClient.faculty.create({
+        data: {
+            name: body.name
+        },
+        select: {
+            name: true,
+            id: true,
+            createdAt: true,
+            updatedAt: true
+        }
+    })
+
+    res.status(200)
+    res.json({
+        ok: true,
+        data: faculty,
+        error: null
+    })
+})
+
+FacultyRoute.use("/", FacultyIDRoute)
+
+export default FacultyRoute
