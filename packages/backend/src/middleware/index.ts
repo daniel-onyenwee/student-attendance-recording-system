2c86d2ab747e8dee737acc4b0c68bb66abf2f47c
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-23 11:49:45 +0100

    ➕ add middleware -> idValidator

--- packages/backend/src/middleware/index.ts
+++ packages/backend/src/middleware/index.ts
@@ -1,3 +1,4 @@
 export { default as auth } from "./auth.js"
 export { default as authAccess } from "./auth-access.js"
 export { default as notFound } from "./not-found.js"
+export { default as idValidator } from "./id-validator.js"
