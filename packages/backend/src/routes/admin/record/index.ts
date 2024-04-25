6f3f98605f590321abcc6ba0298a06b72d90f030
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-25 21:51:00 +0100

    ➕ add route -> student route to record route

--- packages/backend/src/routes/admin/record/index.ts
+++ packages/backend/src/routes/admin/record/index.ts
@@ -3,6 +3,7 @@ import FacultyRoute from "./faculty/index.js"
 import DepartmentRoute from "./department/index.js"
 import CourseRoute from "./course/index.js"
 import LecturerRoute from "./lecturer/index.js"
+import StudentRoute from "./student/index.js"
 
 const RecordRoute = express.Router()
 
@@ -13,5 +14,7 @@ RecordRoute.use("/department", DepartmentRoute)
 RecordRoute.use("/course", CourseRoute)
 
 RecordRoute.use("/lecturer", LecturerRoute)
+
+RecordRoute.use("/student", StudentRoute)
 
 export default RecordRoute
