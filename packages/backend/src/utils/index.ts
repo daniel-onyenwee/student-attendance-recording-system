d25e5d3f1bdedf0e909f70fe944820388e51596c
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-28 00:20:26 +0100

    ➕ add utils -> faceDetectionNet and faceDetectionOptions

--- packages/backend/src/utils/index.ts
+++ packages/backend/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as prismaClient } from "./prisma-client.js"
 export { default as removeSpecialChar } from "./remove-special-char.js"
+export { faceDetectionNet, faceDetectionOptions } from "./face-detection.js"
