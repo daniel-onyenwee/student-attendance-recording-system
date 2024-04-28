d9cee6c9ab74e5ecdefdb9aec814f4eeebb5e681
daniel-onyenwee <danielonyenwee@gmail.com>
2024-04-28 02:06:35 +0100

    🔃 load AI models

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,5 +1,7 @@
 import AppRoute from "./app.js"
+import { faceDetectionNet } from "./utils/index.js"
 
-AppRoute.listen(8080, () => {
+AppRoute.listen(8080, async () => {
+    await faceDetectionNet.loadFromDisk("./models")
     console.log("Starting server at http://localhost:8080")
 })
