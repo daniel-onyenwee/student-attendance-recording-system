8293735748c026d4b7c6563c6b817e1305094a5a
daniel-onyenwee <danielonyenwee@gmail.com>
2024-05-15 02:11:22 +0100

    ➕ add route ->  classAttendance  route to attendance route

--- packages/backend/src/routes/admin/attendance/index.ts
+++ packages/backend/src/routes/admin/attendance/index.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import RegisterRoute from "./register/index.js"
+import ClassAttendanceRoute from "./class-attendance/index.js"
 
 const AttendanceRoute = express.Router()
 
 AttendanceRoute.use("/register", RegisterRoute)
+
+AttendanceRoute.use("/class-attendance", ClassAttendanceRoute)
 
 export default AttendanceRoute
