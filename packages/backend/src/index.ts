a0e62d993ebc36e75e33cc05d4d5e8e257dc253a
daniel-onyenwee <danielonyenwee@gmail.com>
2024-06-22 23:24:59 +0100

    ⬆️ export App route as a function -> initializeApp

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,3 +1,19 @@
 import AppRoute from "./app.js"
+import { PrismaClient } from "@prisma/client"
 
-AppRoute.listen(8080, () => console.log("Starting server at http://localhost:8080"))
+const defaultLogger = (method: string, route: string) => {
+    console.log(`Visited [${method.toUpperCase()}] - ${route}`)
+}
+
+export default function initializeApp(databaseUrl: string, logger?: (method: string, route: string) => void, port: number = 8080) {
+    AppRoute.set("prisma-client", new PrismaClient({ datasourceUrl: databaseUrl }))
+    AppRoute.set("logger", logger)
+
+    AppRoute.listen(port, () => console.log(`Starting server at http://localhost:${port}`))
+}
+
+export {
+    defaultLogger as logger
+}
+
+initializeApp("postgresql://postgres:password@localhost:5432/sar_db?schema=public")
