8f62ff311421b3065f5607213014844c26040f5b
daniel-onyenwee <danielonyenwee@gmail.com>
2024-06-23 00:24:59 +0100

    🔄️ refactor route to return the access token expire period

--- packages/backend/src/routes/auth/reset-token.ts
+++ packages/backend/src/routes/auth/reset-token.ts
@@ -1,11 +1,18 @@
 import express from "express"
 import "dotenv/config"
 import jwt from "jsonwebtoken"
+import { addDays, subHours } from "date-fns"
 
 const ResetTokenRoute = express.Router()
 
 ResetTokenRoute.post("/", (req, res) => {
     let refreshToken = req.app.get("user-refresh-token")
+    /**
+     * 1 hour is subtract from expire date to ensure 
+     * the client request for a new access token before
+     * it current access token expire
+     */
+    let expiresIn = subHours(addDays(new Date(), 1), 1).getTime()
     let userId = req.app.get("user-id")
     let accessToken = jwt.sign({ id: userId }, process.env.ACCESS_TOKEN_SECRET || "secret", { expiresIn: "1d" })
 
@@ -14,7 +21,8 @@ ResetTokenRoute.post("/", (req, res) => {
         ok: true,
         data: {
             refreshToken,
-            accessToken
+            accessToken,
+            expiresIn
         },
         error: null
     })
