62b2dbb031fe7d98a65f47b68d29aed88ca96941
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-06 13:18:19 +0100

    ➕ add dialogs ->create-admin, delete-admin and edit-admin

---
+++ packages/frontend/src/lib/components/dialog/index.ts
@@ -0,0 +1,3 @@
+export { default as DeleteAdminDialog } from "./delete-admin.svelte"
+export { default as EditAdminDialog } from "./edit-admin.svelte"
+export { default as CreateAdminDialog } from "./create-admin.svelte"
