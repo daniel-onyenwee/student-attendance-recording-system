9909570f62a0b9417ec8f63269b5f4c9fcdac2df
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-20 22:13:09 +0100

    ➕ add types -> FacultyModel and DepartmentModel

--- packages/frontend/src/lib/service/admin/types.d.ts
+++ packages/frontend/src/lib/service/admin/types.d.ts
@@ -1,8 +1,26 @@
-export interface AdminUserModel {
+import type { Level } from "../type"
+
+interface BaseModel {
+    updatedAt: Date
+    createdAt: Date
+    metadata: { [name: string]: any }
+}
+
+export interface AdminUserModel extends BaseModel {
     id: string
     refreshToken: string
     password: string
     username: string
-    createdAt: string
-    updatedAt: string
+}
+
+export interface FacultyModel extends BaseModel {
+    name: string
+    id: string
+}
+
+export interface DepartmentModel extends BaseModel {
+    name: string
+    id: string
+    faculty: string
+    levels: Level[]
 }
