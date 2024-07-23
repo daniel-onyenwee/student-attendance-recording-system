870bdfb3ce8d02733a3c4f2b307ac5d7b3cfc498
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-23 22:44:10 +0100

    🔄️ refactor web-worker -> sort

--- packages/frontend/src/lib/web-workers/sort.ts
+++ packages/frontend/src/lib/web-workers/sort.ts
@@ -1,66 +1,28 @@
-import type {
-    FacultyModel,
-    FacultySortByOption,
-    DepartmentModel,
-    DepartmentSortByOption
-} from "@/service"
 import { sort } from "fast-sort"
 
-type SortMessageEventMode = "REQUEST" | "RESPONSE"
 
 type SortMessageEvent = {
-    mode: SortMessageEventMode
-    type: "FACULTY"
-    payload: FacultyModel[]
-    sortOptions: FacultySortByOption
-} | {
-    mode: SortMessageEventMode
-    type: "DEPARTMENT"
-    payload: DepartmentModel[]
-    sortOptions: DepartmentSortByOption
+    mode: "REQUEST" | "RESPONSE"
+    type: string
+    payload: { [name: string]: any }[]
+    sortOptions: {
+        by: string
+        ascending: boolean
+    }
 }
 
 addEventListener('message', (e: MessageEvent<SortMessageEvent>) => {
     const { type, sortOptions, payload, mode } = e.data
 
+    if (!sortOptions.by && mode != "REQUEST") return
 
-    if (type == "FACULTY" && mode == "REQUEST") {
-        let result = payload
-        if (sortOptions.by == "name") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.name)
-                : sort(payload).desc((i) => i.name);
-        } else if (sortOptions.by == "createdAt") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.createdAt)
-                : sort(payload).desc((i) => i.createdAt);
-        } else if (sortOptions.by == "updatedAt") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.updatedAt)
-                : sort(payload).desc((i) => i.updatedAt);
-        }
-
-        postMessage({ type: "FACULTY", mode: "RESPONSE", payload: result })
-    } else if (type == "DEPARTMENT" && mode == "REQUEST") {
-        let result = payload
-        if (sortOptions.by == "name") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.name)
-                : sort(payload).desc((i) => i.name);
-        } else if (sortOptions.by == "createdAt") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.createdAt)
-                : sort(payload).desc((i) => i.createdAt);
-        } else if (sortOptions.by == "updatedAt") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.updatedAt)
-                : sort(payload).desc((i) => i.updatedAt);
-        } else if (sortOptions.by == "faculty") {
-            result = sortOptions.ascending
-                ? sort(payload).asc((i) => i.faculty)
-                : sort(payload).desc((i) => i.faculty);
-        }
-
-        postMessage({ type: "DEPARTMENT", mode: "RESPONSE", payload: result })
+    let result = payload
+    let payloadKeys = Object.keys(result[0] || {})
+    if (payloadKeys.includes(sortOptions.by)) {
+        result = sortOptions.ascending
+            ? sort(payload).asc((i) => i[sortOptions.by])
+            : sort(payload).desc((i) => i[sortOptions.by]);
     }
+
+    postMessage({ type, mode: "RESPONSE", payload: result })
 })
