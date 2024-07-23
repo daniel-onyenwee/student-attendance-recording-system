5f3222ff6e4a5c7aa387dcf93e71961386a13754
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-23 22:46:47 +0100

    ➕ add dialog -> CourseRecordDialog

---
+++ packages/frontend/src/lib/components/dialog/course-record.svelte
@@ -0,0 +1,429 @@
+<script lang="ts">
+  import { LoaderCircle, ChevronsUpDown, Check } from "lucide-svelte/icons";
+  import { showDialogToast } from "@/utils";
+  import * as Sheet from "@/components/ui/sheet";
+  import * as Command from "@/components/ui/command";
+  import * as Popover from "@/components/ui/popover";
+  import {
+    createCourse,
+    updateCourse,
+    getDepartments,
+    type DepartmentModel,
+    type CourseModel,
+  } from "@/service";
+  import { cn } from "@/utils.js";
+  import { Button } from "@/components/ui/button";
+  import { createEventDispatcher, tick } from "svelte";
+  import { Label } from "@/components/ui/label";
+  import { Input } from "@/components/ui/input";
+
+  export let accessToken: string;
+
+  function show(mode: "CREATE", data: undefined): void;
+  function show(mode: "UPDATE", data: CourseModel): void;
+  export function show(mode: "CREATE" | "UPDATE", data: any) {
+    dialogMode = mode;
+    if (mode == "UPDATE") {
+      courseData = structuredClone(data);
+    }
+    open = true;
+  }
+
+  export function close() {
+    internalClose();
+    open = false;
+  }
+
+  function internalClose() {
+    errorMessage = {};
+    courseData = {};
+  }
+
+  function onSemesterSelected(currentValue: string) {
+    courseData.semester = currentValue as any;
+    semesterPopoverOpen = false;
+  }
+
+  function onLevelSelected(currentValue: string) {
+    courseData.level = `L_${currentValue}` as any;
+    levelsPopoverOpen = false;
+  }
+
+  function onDepartmentSelected(departmentName: string) {
+    courseData.department = departmentName;
+    departmentPopoverOpen = false;
+    departmentsLoaded = false;
+    departments = [];
+  }
+
+  async function onDepartmentPopoverOpened(open?: boolean) {
+    if (open) {
+      try {
+        departments =
+          (await getDepartments({ accessToken, count: "all" })).data || [];
+      } catch (error) {
+        departments = [];
+      } finally {
+        departmentsLoaded = true;
+      }
+    }
+  }
+
+  function closeAndFocusTrigger(triggerId: string) {
+    tick().then(() => {
+      document.getElementById(triggerId)?.focus();
+    });
+  }
+
+  async function onCreateOrUpdate() {
+    requestOngoing = true;
+
+    errorMessage = {};
+
+    let formInputs = ["title", "code", "level", "semester", "department"];
+
+    for (const key of formInputs) {
+      if (!Object(courseData)[key]) {
+        Object(errorMessage)[key] = "Field required";
+        requestOngoing = false;
+        return;
+      }
+    }
+
+    try {
+      let serviceRequest = null;
+      let { departmentId, title, code, level, semester } =
+        courseData as Required<CourseModel & { departmentId: string }>;
+
+      if (dialogMode == "CREATE") {
+        serviceRequest = await createCourse({
+          accessToken: accessToken,
+          departmentId,
+          title,
+          code,
+          level,
+          semester,
+        });
+      } else {
+        serviceRequest = await updateCourse({
+          accessToken: accessToken,
+          id: courseData.id as string,
+          departmentId,
+          title,
+          code,
+          level,
+          semester,
+        });
+      }
+
+      if (!serviceRequest) {
+        throw new Error();
+      }
+
+      if (serviceRequest.error) {
+        requestOngoing = false;
+        if (
+          serviceRequest.error.code >= 1001 &&
+          serviceRequest.error.code < 1004
+        ) {
+          close();
+          dispatch("onSessionError");
+        } else if (serviceRequest.error.code == 3006) {
+          errorMessage.department = serviceRequest.error.message;
+        } else if (
+          serviceRequest.error.code == 3011 ||
+          serviceRequest.error.code == 3014
+        ) {
+          errorMessage.level = serviceRequest.error.message;
+        } else if (serviceRequest.error.code == 3010) {
+          errorMessage.code = serviceRequest.error.message;
+        } else if (serviceRequest.error.code == 3012) {
+          errorMessage.faculty = serviceRequest.error.message;
+        } else if (serviceRequest.error.code == 3013) {
+          errorMessage.code = "Course code already exist";
+        } else {
+          close();
+          showDialogToast(
+            "ERROR",
+            "Request failed",
+            serviceRequest.error.message
+          );
+        }
+        return;
+      }
+
+      showDialogToast(
+        "SUCCESS",
+        "Request successfully",
+        `Course successfully ${dialogMode == "CREATE" ? "created" : "edited"}`
+      );
+      dispatch("onSuccessful");
+    } catch {
+      showDialogToast("ERROR", "Request failed", "Unexpected error");
+    }
+
+    requestOngoing = false;
+    close();
+  }
+
+  let requestOngoing: boolean = false;
+  let errorMessage: Partial<Record<keyof CourseModel, string>> = {};
+  let courseData: Partial<CourseModel & { departmentId: string }> = {};
+  let open = false;
+  let levelsPopoverOpen = false;
+  let semesterPopoverOpen = false;
+  let departmentPopoverOpen = false;
+  let departmentsLoaded = false;
+  let departments: DepartmentModel[] = [];
+  let dialogMode: "CREATE" | "UPDATE" = "CREATE";
+  let dispatch = createEventDispatcher();
+
+  $: dialogTitle = `${dialogMode == "CREATE" ? "Create" : "Edit"} course`;
+  $: dialogDescription = `${
+    dialogMode == "CREATE"
+      ? "Create a new course here. Click create when you're done."
+      : "Make changes to the course here. Click save when you're done."
+  }`;
+</script>
+
+<Sheet.Root
+  closeOnEscape={!requestOngoing}
+  closeOnOutsideClick={!requestOngoing}
+  bind:open
+  onOpenChange={(open) => {
+    if (!open) {
+      internalClose();
+    }
+  }}
+>
+  <Sheet.Content side="right" class="overflow-auto">
+    <Sheet.Header>
+      <Sheet.Title>{dialogTitle}</Sheet.Title>
+      <Sheet.Description>{dialogDescription}</Sheet.Description>
+    </Sheet.Header>
+    <form class="grid items-start gap-4 mt-4">
+      <div class="grid gap-2">
+        <Label for="title">Title</Label>
+        <Input
+          placeholder="Course title"
+          type="text"
+          id="title"
+          bind:value={courseData.title}
+        />
+        <p
+          class="text-sm font-medium text-red-600 {!errorMessage.title &&
+            'hidden'}"
+        >
+          {errorMessage.title}
+        </p>
+      </div>
+      <div class="grid gap-2">
+        <Label for="code">Code</Label>
+        <Input
+          placeholder="Course code"
+          type="text"
+          id="code"
+          bind:value={courseData.code}
+        />
+        <p
+          class="text-sm font-medium text-red-600 {!errorMessage.code &&
+            'hidden'}"
+        >
+          {errorMessage.code}
+        </p>
+      </div>
+      <div class="grid gap-2">
+        <Label for="department">Department</Label>
+        <Popover.Root
+          bind:open={departmentPopoverOpen}
+          onOpenChange={(open) => onDepartmentPopoverOpened(open)}
+          let:ids
+        >
+          <Popover.Trigger asChild let:builder>
+            <Button
+              builders={[builder]}
+              variant="outline"
+              role="combobox"
+              aria-expanded={departmentPopoverOpen}
+              class="w-full justify-between font-normal {courseData.department ==
+                undefined && 'text-muted-foreground'}"
+            >
+              {#if courseData.department}
+                {courseData.department}
+              {:else}
+                Select department
+              {/if}
+              <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content class="p-0" style="width: calc(100% - 3rem)">
+            <Command.Root loop>
+              <Command.Input placeholder="Search department..." />
+              <Command.List>
+                {#if departmentsLoaded}
+                  <Command.Empty>No department found.</Command.Empty>
+                  <Command.Group>
+                    {#each departments as department}
+                      <Command.Item
+                        onSelect={(currentValue) => {
+                          courseData.departmentId = department.id;
+                          onDepartmentSelected(currentValue);
+                          closeAndFocusTrigger(ids.trigger);
+                        }}
+                        value={department.name}
+                      >
+                        <Check
+                          class={cn(
+                            "mr-2 h-4 w-4",
+                            courseData.department !== department.name &&
+                              "text-transparent"
+                          )}
+                        />
+                        {department.name}
+                      </Command.Item>
+                    {/each}
+                  </Command.Group>
+                {:else}
+                  <Command.Loading class="py-6 text-center text-sm">
+                    Loading departments..
+                  </Command.Loading>
+                {/if}
+              </Command.List>
+            </Command.Root>
+          </Popover.Content>
+        </Popover.Root>
+        <p
+          class="text-sm font-medium text-red-600 {!errorMessage.department &&
+            'hidden'}"
+        >
+          {errorMessage.department}
+        </p>
+      </div>
+      <div class="grid gap-2">
+        <Label for="semester">Semester</Label>
+        <Popover.Root bind:open={semesterPopoverOpen} let:ids>
+          <Popover.Trigger asChild let:builder>
+            <Button
+              builders={[builder]}
+              variant="outline"
+              role="combobox"
+              aria-expanded={semesterPopoverOpen}
+              class="w-full justify-between font-normal {courseData.semester ==
+                undefined && 'text-muted-foreground'}"
+            >
+              {#if courseData.semester}
+                {courseData.semester}
+              {:else}
+                Select semester
+              {/if}
+              <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content class="p-0" style="width: calc(100% - 3rem)">
+            <Command.Root loop>
+              <Command.Input placeholder="Search semester..." />
+              <Command.List>
+                <Command.Empty>No semester found.</Command.Empty>
+                <Command.Group>
+                  {#each ["FIRST", "SECOND"] as semester}
+                    <Command.Item
+                      onSelect={(currentValue) => {
+                        onSemesterSelected(currentValue);
+                        closeAndFocusTrigger(ids.trigger);
+                      }}
+                      value={semester}
+                    >
+                      <Check
+                        class={cn(
+                          "mr-2 h-4 w-4",
+                          courseData.semester !== semester && "text-transparent"
+                        )}
+                      />
+                      {semester}
+                    </Command.Item>
+                  {/each}
+                </Command.Group>
+              </Command.List>
+            </Command.Root>
+          </Popover.Content>
+        </Popover.Root>
+        <p
+          class="text-sm font-medium text-red-600 {!errorMessage.semester &&
+            'hidden'}"
+        >
+          {errorMessage.semester}
+        </p>
+      </div>
+      <div class="grid gap-2">
+        <Label for="level">Level</Label>
+        <Popover.Root bind:open={levelsPopoverOpen} let:ids>
+          <Popover.Trigger asChild let:builder>
+            <Button
+              builders={[builder]}
+              variant="outline"
+              role="combobox"
+              aria-expanded={levelsPopoverOpen}
+              class="w-full justify-between font-normal {courseData.level ==
+                undefined && 'text-muted-foreground'}"
+            >
+              {#if courseData.level}
+                {courseData.level.replace("L_", String())}L
+              {:else}
+                Select level
+              {/if}
+              <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </Popover.Trigger>
+          <Popover.Content class="p-0" style="width: calc(100% - 3rem)">
+            <Command.Root loop>
+              <Command.Input placeholder="Search level..." />
+              <Command.List>
+                <Command.Empty>No level found.</Command.Empty>
+                <Command.Group>
+                  {#each { length: 10 } as _, i}
+                    <Command.Item
+                      onSelect={(currentValue) => {
+                        onLevelSelected(currentValue);
+                        closeAndFocusTrigger(ids.trigger);
+                      }}
+                      value={`${(i + 1) * 100}`}
+                    >
+                      <Check
+                        class={cn(
+                          "mr-2 h-4 w-4",
+                          courseData.level !== `L_${(i + 1) * 100}` &&
+                            "text-transparent"
+                        )}
+                      />
+                      {(i + 1) * 100}L
+                    </Command.Item>
+                  {/each}
+                </Command.Group>
+              </Command.List>
+            </Command.Root>
+          </Popover.Content>
+        </Popover.Root>
+        <p
+          class="text-sm font-medium text-red-600 {!errorMessage.level &&
+            'hidden'}"
+        >
+          {errorMessage.level}
+        </p>
+      </div>
+      <Button
+        type="submit"
+        on:click={onCreateOrUpdate}
+        disabled={requestOngoing}
+      >
+        <LoaderCircle
+          class="mr-2 h-4 w-4 animate-spin {!requestOngoing && 'hidden'}"
+        />
+        {!requestOngoing
+          ? dialogMode == "CREATE"
+            ? "Create"
+            : "Save"
+          : "Please wait"}
+      </Button>
+    </form>
+  </Sheet.Content>
+</Sheet.Root>
