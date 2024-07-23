3874b799178eee74b2776efcd9bf6e2a1b31d69e
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-23 22:43:06 +0100

    ➕ add property "id" to "BaseModel" interface

--- packages/frontend/src/lib/service/admin/types.d.ts
+++ packages/frontend/src/lib/service/admin/types.d.ts
@@ -1,6 +1,7 @@
 import type { Level } from "../type"
 
 interface BaseModel {
+    id: string
     updatedAt: Date
     createdAt: Date
     metadata: { [name: string]: any }
@@ -15,12 +16,19 @@ export interface AdminUserModel extends BaseModel {
 
 export interface FacultyModel extends BaseModel {
     name: string
-    id: string
 }
 
 export interface DepartmentModel extends BaseModel {
     name: string
-    id: string
     faculty: string
     levels: Level[]
+}
+
+export interface CourseModel extends BaseModel {
+    faculty: string
+    department: string
+    semester: Semester
+    level: Level
+    code: string
+    title: string
 }
