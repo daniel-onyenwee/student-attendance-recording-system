dd73d31adea147b6fe211d6aeea6336c87f75a0c
daniel-onyenwee <danielonyenwee@gmail.com>
2024-07-24 14:31:56 +0100

    ➕ add type -> App.FilterByScheme

--- packages/frontend/src/app.d.ts
+++ packages/frontend/src/app.d.ts
@@ -2,6 +2,15 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		type FilterByScheme =
+			| {
+				type: "text" | "number";
+			}
+			| {
+				type: "select";
+				options: Array<{ label?: string; value: string }> | Array<string>;
+			};
+
 		// interface Error {}
 		interface Locals {
 			session: {
