259feec9332e6f8cdec34c58677b1809ed7c454c
daniel-onyenwee <danielonyenwee@gmail.com>
2024-08-06 15:02:22 +0100

    ➕ add utility -> remove-underscore and get-current-session

--- packages/frontend/src/lib/utils/index.ts
+++ packages/frontend/src/lib/utils/index.ts
@@ -1,3 +1,5 @@
 export { default as showDialogToast } from "./show-dialog-toast.js"
 export { default as sleep } from "./sleep.js"
+export { default as removeUnderscore } from "./remove-underscore.js"
 export { default as capitalizeText } from "./capitalize-text.js"
+export { default as getCurrentSession } from "./get-current-session.js"
