41be3dddc7d08108e4987ff9fd0fe0af70842dca
daniel-onyenwee <danielonyenwee@gmail.com>
2024-08-06 15:01:37 +0100

    🔄️ refactor utility to support capitalizing only the first word

--- packages/frontend/src/lib/utils/capitalize-text.ts
+++ packages/frontend/src/lib/utils/capitalize-text.ts
@@ -1,5 +1,10 @@
-export default function capitalizeText(text: string): string {
-    return text.split(' ').map(word => {
+export default function capitalizeText(text: string, firstWordOnly: boolean = false): string {
+    return text.split(' ').map((word, index) => {
+        if (firstWordOnly && index == 0) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        } else if (firstWordOnly) {
+            return word
+        }
         return word.charAt(0).toUpperCase() + word.slice(1);
     }).join(' ');
 }
