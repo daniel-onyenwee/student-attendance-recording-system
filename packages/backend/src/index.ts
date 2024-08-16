6f2967187cd891fa173e6343147faa9bce6dbf20
daniel-onyenwee <danielonyenwee@gmail.com>
2024-08-16 01:07:41 +0100

    ➕ add functions -> App and initializeBackend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,22 +1,25 @@
 import AppRoute from "./app.js"
 import { PrismaClient } from "@prisma/client"
+import express, { Express } from "express"
 
-export default function initializeApp(databaseUrl: string, logger?: (type: "START" | "ROUTE" | "CLOSE", message: { [key: string]: any }) => void, port: number = 8080) {
-    AppRoute.set("prisma-client", new PrismaClient({ datasourceUrl: databaseUrl }))
+export default function App(databaseUrl: string, logger?: (type: "START" | "ROUTE" | "CLOSE", message: { [key: string]: any }) => void, port: number = 8080) {
+    let app = express()
+
+    app.set("prisma-client", new PrismaClient({ datasourceUrl: databaseUrl }))
 
     const routeLogger = (method: string, path: string) => {
         logger ? logger("ROUTE", { method, path }) : void 0
     }
 
-    AppRoute.set("logger", routeLogger)
+    app.set("logger", routeLogger)
 
-    AppRoute.listen(port, () => {
-        logger ? logger("START", { port }) : void 0
-    })
+    app.use(AppRoute)
+
+    return app
 }
 
-initializeApp("postgresql://postgres:password@localhost:5432/sar_db?schema=public", (type, message) => {
-    if (type == "START") {
-        console.log(`Starting at http://localhost:${message.port}`)
-    }
-})
+export function initializeBackend(app: Express, databaseUrl: string, routePath: string = "/api") {
+    app.set("prisma-client", new PrismaClient({ datasourceUrl: databaseUrl }))
+
+    app.use(routePath, AppRoute)
+}
